0682aca88ca62eda662e8506ccb530b3e47743d1
vaishnavihole <vaishnavihole1@gmail.com>
2024-05-05 12:05:20 +0530

    feat: user model design

---
+++ server/models/User.js
@@ -0,0 +1,34 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+   name:{
+         type: String,
+        required: true
+   },
+   email:{
+            type: String,
+            required: true
+    },
+   mobile:{
+            type: String,
+            required: true
+    },
+    city:{
+            type: String,
+            required: true
+    },
+    password:{
+            type: String,
+            required: true
+    },
+    role:{
+            type: String,
+            enum: ['admin', 'user', 'influencer'],
+    },
+   
+}, {
+    timestamps: true 
+});
+const User = model("User", userSchema);
+
+export default User;
