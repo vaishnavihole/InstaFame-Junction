a24fda18adf82c2f76d714b9f167d2591861703f
vaishnavihole <vaishnavihole1@gmail.com>
2024-05-07 12:29:10 +0530

    design home page header

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import './Home.css';  
 import Navbar from '../../components/Navbar/Navbar';
+import ImgHeader from './HeaderImg.png';
 
 function Home() {
   return (
     <div>
         <Navbar />
+        <div className='row'>
+        <div className='col-md-6'>
+        <div className='header-img-container'><img className='header-image' src={ImgHeader} alt="Mobile View" /></div>
+        </div>
+        <div className='col-md-6'>
+          <div className='features-container'>
+            <h1 className='quotes'>Get Your Influencer <br/> Without Any Efforts</h1>
+          </div>
+          <div className="button-container">
+            <button className="influencer-btn influencer-btn-want">I want an influencer</button>
+            <button className="influencer-btn">I want to be an influencer</button>
+        </div>
+        </div>
+      </div>
+
     </div>
   )
 }
