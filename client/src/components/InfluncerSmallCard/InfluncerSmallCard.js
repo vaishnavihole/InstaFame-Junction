2c2d8b8a1004cd1cbf8dd85be1e0b3c0e0f462c1
vaishnavihole <vaishnavihole1@gmail.com>
2024-05-11 10:40:35 +0530

    add changes in cards

--- client/src/components/InfluncerSmallCard/InfluncerSmallCard.js
+++ client/src/components/InfluncerSmallCard/InfluncerSmallCard.js
@@ -5,8 +5,8 @@ import { FaInstagram } from 'react-icons/fa';
 
 function InfluncerSmallCard() {
   return (
-    <div>
-     <div className='influncer-container'>
+  <div>
+<div className='influncer-container'>
 <div className="influncer-card">
   <img src={profile} alt="Feature 1" className="influncer-profile-image" />
   <h3 className="user-name">suraj_shende_247</h3>
@@ -14,7 +14,11 @@ function InfluncerSmallCard() {
  <FaInstagram className="instagram-icon" /> 
  <span className="follower-count">Followers: 10k</span>
  </div>
+
+ 
 </div>
+
+
 
 
 
