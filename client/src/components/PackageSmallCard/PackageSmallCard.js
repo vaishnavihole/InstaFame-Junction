1dc059159c71af059ff2e82d9295ef68a8a00bd2
vaishnavihole <vaishnavihole1@gmail.com>
2024-05-28 11:02:52 +0530

    feat: convert packagesmallcard page into tailwind css

--- client/src/components/PackageSmallCard/PackageSmallCard.js
+++ client/src/components/PackageSmallCard/PackageSmallCard.js
@@ -1,41 +1,34 @@
 import React from 'react';
-import './PackageSmallCard.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
+import './PackageSmallCard.css';
 
-const PackageSmallCard = ({_id, packageName, features, price, showLink }) => {
+const PackageSmallCard = ({ _id, packageName, features, price, showLink }) => {
   if (!Array.isArray(features) || features.length === 0) {
-    return null; // Don't render the card if there are no features
+    return null;
   }
 
   return (
-    <div className="card-container">
-      <div className="package-card">
-        <div className="details">
-          <h3 className="package-heading">{packageName}</h3>
-          <ul className="feature-list">
-            {features.map((feature, index) => (
-              <li key={index} className="feature-item">
-                <FontAwesomeIcon icon={faCircleCheck} className="custom-icon" /> {feature}
-              </li>
-            ))}
-          </ul>
-          <div className="price">₹{price}</div>
-          {
-            showLink ? 
-            <Link to={`/addNote/${_id}`} >
-            <button className="buy-now-btn">
-              Buy Now
-            </button>
-            </Link> : null
-          }
-        
-        </div>
+    <div className="flex justify-center">
+      <div className=  "bg-white rounded-2xl p-6 shadow-lg transition-transform duration-300 hover:translate-y-1 hover:shadow-xl w-80 mx-4 my-6 flex flex-col items-center text-center">
+        <h3 className=" package-heading">{packageName}</h3>
+        <ul className="list-none mb-4 overflow-auto flex-grow">
+          {features.map((feature, index) => (
+            <li key={index} className="flex items-center justify-center text-lg text-gray-800 mb-2">
+              <FontAwesomeIcon icon={faCircleCheck} className="text-blue-400 mr-2" /> {feature}
+            </li>
+          ))}
+        </ul>
+        <div className="text-3xl font-semibold mb-4">₹{price}</div>
+        {showLink && (
+          <Link to={`/addNote/${_id}`} >
+           <button className='buy-now-btn'>Buy Now</button>
+          </Link>
+        )}
       </div>
     </div>
   );
 };
 
 export default PackageSmallCard;
-
