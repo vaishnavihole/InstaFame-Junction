1ba1b5a7cfd3573adc472bfc38d2052e577063a2
vaishnavihole <vaishnavihole1@gmail.com>
2024-05-28 10:08:18 +0530

    feat: convert navbar css in tailwind css

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Navbar.css'; // Make sure the CSS file is correctly imported
+import './Navbar.css';
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -9,25 +9,23 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="navbar">
-      <div className="navbar-left">
+    <nav className="navbar flex justify-between items-center px-4 py-2 text-white md:h-16 relative z-50">
+      <div className="navbar-left flex items-center">
         <div className="logo">
-          {/* Your logo goes here */}
+
           {/* <img src="logo.png" alt="Logo" /> */}
         </div>
-        <div className="brand-name">
-          InstaFame Junction
-        </div>
+        <div className="brand-name font-bold text-lg">InstaFame Junction</div>
       </div>
-      <div className="navbar-right">
-        <div className="menu-toggle" onClick={toggleMenu}>
-          <i className="fa fa-bars"></i>
+      <div className="navbar-right flex items-center">
+        <div className="menu-toggle cursor-pointer text-white text-2xl md:hidden" onClick={toggleMenu}>
+          <i className="fas fa-bars"></i>
         </div>
-        <ul className={`menu-items ${isMenuOpen ? 'active' : ''}`}>
-          <li><a href="/">Home</a></li>
-          <li><a href="/contactUs">Contact</a></li>
-          <li><a href="/myProfile">My Profile</a></li>
-          <li><a href="/login">Login</a></li>
+        <ul className={`menu-items flex flex-col md:flex-row md:flex ${isMenuOpen ? 'block' : 'hidden'}`}>
+          <li className="my-1 md:my-0 md:mx-2"><a href="/" className="text-white hover:underline">Home</a></li>
+          <li className="my-1 md:my-0 md:mx-2"><a href="/contactUs" className="text-white hover:underline">Contact</a></li>
+          <li className="my-1 md:my-0 md:mx-2"><a href="/myProfile" className="text-white hover:underline">My Profile</a></li>
+          <li className="my-1 md:my-0 md:mx-2"><a href="/login" className="text-white hover:underline">Login</a></li>
         </ul>
       </div>
     </nav>
