8cc4749797ec81e29849c2564f323659eefaa10d
vaishnavihole <vaishnavihole1@gmail.com>
2024-05-29 08:47:18 +0530

    convert addAccountForm css into tailwind

--- client/src/views/AddAccountForm/AddAccountForm.js
+++ client/src/views/AddAccountForm/AddAccountForm.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import './AddAccountForm.css';
+import React, { useState } from 'react';
 import Navbar from '../../components/Navbar/Navbar';
-import Footer from '../../components/Footer/Footer'
+import Footer from '../../components/Footer/Footer';
 import axios from 'axios';
+import './AddAccountForm.css';
 import swal from 'sweetalert2';
 
 function AddAccountForm() {
@@ -11,7 +11,6 @@ function AddAccountForm() {
   const [followers, setFollowers] = useState('');
   const [subscribers, setSubscribers] = useState('');
 
-  
   const getUserId = () => {
     return JSON.parse(sessionStorage.getItem('user'))._id;
   };
@@ -20,46 +19,41 @@ function AddAccountForm() {
     e.preventDefault();
     const userId = getUserId(); 
     try {
-      const response = await axios.post('/api/v1/addAccount', {
+      await axios.post('/api/v1/addAccount', {
         accountType,
         handle,
         followers,
         subscribers,
-        userId: userId, 
+        userId, 
       });
       swal.fire({
         icon: 'success',
         title: 'Account Added!',
         text: 'Your account has been added successfully.',
       });
-
       window.location.href = '/myAccounts';
-
-      
-     
     } catch (error) {
       swal.fire({
         icon: 'error',
         title: 'Error',
         text: 'An error occurred. Please try again later.',
       });
-      
     }
   };
 
   return (
     <div>
       <Navbar />
-      <div className="add-account-form-container">
-        <h2 className="form-title">Add Account</h2>
-        <form onSubmit={handleSubmit} className="add-account-form">
-          <div className="form-group account-type-group">
-            <label htmlFor="accountType" className="form-label custom-label">Account</label>
+      <div className="max-w-md mx-auto mt-12 p-6 text-center">
+        <h2 className="form-title text-4xl font-bold mb-8">Add Account</h2>
+        <form onSubmit={handleSubmit} className="flex flex-col space-y-6">
+          <div className="form-group">
+            <label htmlFor="accountType" className="block mb-2 text-gray-700">Account</label>
             <select
               id="accountType"
               value={accountType}
               onChange={(e) => setAccountType(e.target.value)}
-              className="form-input custom-input select-input"
+              className="small-input p-2 text-lg border border-gray-300 rounded-lg transition duration-300 focus:outline-none focus:border-blue-500"
               required
             >
               <option value="">Select Account</option>
@@ -70,44 +64,45 @@ function AddAccountForm() {
               <option value="youtube">YouTube</option>
             </select>
           </div>
-      
-          <div className="form-group name-group">
-            <label htmlFor="handle" className="form-label custom-label">Handle Name</label>
+          <div className="form-group">
+            <label htmlFor="handle" className="block mb-2 text-gray-700">Handle Name</label>
             <input
               type="text"
               id="handle"
               value={handle}
               onChange={(e) => setHandle(e.target.value)}
-              className="form-input custom-input text-input"
+              className="small-input w-full p-2 text-lg border border-gray-300 rounded-lg transition duration-300 focus:outline-none focus:border-blue-500"
               required
             />
           </div>
           {accountType !== 'youtube' ? (
-            <div className="form-group followers-group">
-              <label htmlFor="followers" className="form-label custom-label">Followers</label>
+            <div className="form-group">
+              <label htmlFor="followers" className="block mb-2 text-gray-700">Followers</label>
               <input
                 type="number"
                 id="followers"
                 value={followers}
                 onChange={(e) => setFollowers(e.target.value)}
-                className="form-input custom-input number-input"
+                className="small-input w-full p-2 text-lg border border-gray-300 rounded-lg transition duration-300 focus:outline-none focus:border-blue-500"
                 required
               />
             </div>
           ) : (
-            <div className="form-group subscribers-group">
-              <label htmlFor="subscribers" className="form-label custom-label">Subscribers</label>
+            <div className="form-group">
+              <label htmlFor="subscribers" className="block mb-2 text-gray-700">Subscribers</label>
               <input
                 type="number"
                 id="subscribers"
                 value={subscribers}
                 onChange={(e) => setSubscribers(e.target.value)}
-                className="form-input custom-input number-input"
+                className="small-input w-full p-2 text-lg border border-gray-300 rounded-lg transition duration-300 focus:outline-none focus:border-blue-500"
                 required
               />
             </div>
           )}
-          <button type="submit" className="form-submit-button">Add Account</button>
+          <button type="submit" className="form-submit-button">
+            Add Account
+          </button>
         </form>
       </div>
       <Footer />
