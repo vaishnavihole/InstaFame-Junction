85ee523137cd78c51a27f2011ee21ebda709b1d6
vaishnavihole <vaishnavihole1@gmail.com>
2024-05-30 08:18:28 +0530

    css added

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,36 +1,35 @@
 import React from 'react';
 import './Footer.css';
-import livinqr from './logo-livin.png';
+import instaFameLogo from './instaFameLogo.png';
 
 function Footer() {
   return (
     <footer>
-    <div class="footer-content">
-        <div class="footer-logo">
-            <img src={livinqr} alt="Company Logo" />
+      <div className="footer-content">
+        <div className="footer-logo">
+          <img src={instaFameLogo} alt="Company Logo" className="footer-logo-img" />
         </div>
-        <div class="footer-links">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Contact Us</a></li>
-            </ul>
+        <div className="footer-links">
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
         </div>
-        <div class="footer-social">
-            <ul>
-                <li><a href="https://twitter.com/vaishnavi_hole"><i class="fab fa-twitter"></i></a></li>
-                <li><a href="https://instagram.com/vaishnavi_hole_29"><i class="fab fa-instagram"></i></a></li>
-                <li><a href="https://www.linkedin.com/company/vaishnavihole"><i class="fab fa-linkedin-in"></i></a></li>
-            </ul>
+        <div className="footer-social">
+          <ul>
+            <li><a href="https://twitter.com/vaishnavi_hole"><i className="fab fa-twitter"></i></a></li>
+            <li><a href="https://instagram.com/vaishnavi_hole_29"><i className="fab fa-instagram"></i></a></li>
+            <li><a href="https://www.linkedin.com/company/vaishnavihole"><i className="fab fa-linkedin-in"></i></a></li>
+          </ul>
         </div>
-    </div>
-    <div class="footer-bottom">
+      </div>
+      <div className="footer-bottom">
         <p>Developed By Vaishnavi Hole❤️</p>
-    </div>
-</footer>
-
-  )
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
